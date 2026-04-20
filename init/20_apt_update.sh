215f8beda100805b66c933d223860e452e891ee9
dlandon <dlandon@familyoflandon.com>
2026-04-20 12:40:22 -0500

    Update 20_apt_update.sh

--- init/20_apt_update.sh
+++ init/20_apt_update.sh
@@ -13,10 +13,14 @@ apt-get update --allow-releaseinfo-change 2>&1 | tee /tmp/test_update
 # Verify that the updates will work.
 #
 if ! grep -q 'Failed' /tmp/test_update; then
+	#
 	# Perform Upgrade
+	#
 	apt-get -y upgrade -o Dpkg::Options::="--force-confold"
 
+	#
 	# Clean + purge old/obsoleted packages
+	#
 	apt-get -y autoremove
 	apt-get clean -y
 else
